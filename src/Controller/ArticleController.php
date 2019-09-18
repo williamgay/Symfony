470988ee1643e0048b150411a57e5eb63f3ea9eb
William Gay <williamgay1962@gmail.com>
2019-09-18 16:24:38 -0400

    Added commenting template(Still hardcoded at this point)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,20 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(){
-        return new Response("My first Symfony page!");
+        return $this->render('article/homepage.html.twig', [
+            'title'=>ucwords('home page')
+        ]);
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug){
         $comments = [
-            "This is a comment",
-            "This is a second comment",
-            "This is a third comment"
+            ["text"=>"This is a comment", "author"=>"William Gay", "time"=>"30 minutes ago", "avatar"=>"https://bootdey.com/img/Content/user_1.jpg"],
+            ["text"=>"This is a second comment", "author"=>"Tammy Montgomery", "time"=>"28 minutes ago", "avatar"=>"https://bootdey.com/img/Content/user_2.jpg"],
+            ["text"=>"This is a third comment", "author"=>"John Doe", "time"=>"45 minutes ago", "avatar"=>"https://bootdey.com/img/Content/user_3.jpg"]
         ];
         return $this->render('article/show.html.twig', [
             'title' =>ucwords(str_replace('-', ' ', $slug)),
